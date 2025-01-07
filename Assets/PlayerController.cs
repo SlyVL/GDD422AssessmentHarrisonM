4d68771f431adb3d2f5e648e57d2e7e769cf40db
UOC\2405962 <2405962@stu.chi.ac.uk>
2025-01-07 11:46:37 +0000

    Created Player movement and map boundaries

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{ //Creating borders on the map so the player cannot move outside it
+    float xMin = -13;
+    float xMax = 13f;
+    float yMin = -4f;
+    float yMax = 4f;
+    //the player movement speed which can be changed on the inspector 
+    [SerializeField] float speed = 10f;
+   
+   
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+   
+
+    private void Update()
     {
+        //Allows me to put lines of code else where in a different private void
+        Moving();
         
     }
+
+    private void Moving()
+    {
+        //Retrieving the player input for horizontal movement and moving the sprite by speed which was set prior to this and Time.deltaTime
+        var xInput = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        var newXPos = Mathf.Clamp(transform.position.x + xInput, xMin, xMax);
+
+        //local vars for vertical movement
+        var yInput = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        var newYPos = Mathf.Clamp(transform.position.y + yInput, yMin, yMax);
+        //updating the position of the player
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+
+   
+
+   
 }
