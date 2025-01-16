4688fe63e1a180a448c6777979b72c1de5f71b3f
DESKTOP-EDLB50K\littl <harrisonmuldowney06@gmail.com>
2025-01-16 13:02:56 +0000

    Created main menu with music

---
+++ Assets/FadeManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeManager : MonoBehaviour
+{
+    public static FadeManager Instance;  // Singleton instance
+    public Image fadeImage; // UI Image component for fading effect
+    public float fadeDuration = 1f; // Duration of the fade transition
+
+    private void Awake()
+    {
+        // Ensure that there is only one instance of FadeManager in the scene
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Don't destroy this object when loading a new scene
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Call this to fade out and load a new scene
+    public void FadeOut(string sceneName)
+    {
+        StartCoroutine(FadeAndLoadScene(sceneName));
+    }
+
+    private IEnumerator FadeAndLoadScene(string sceneName)
+    {
+        // Start fading out (fade to black)
+        float timeElapsed = 0f;
+        Color startColor = fadeImage.color;
+        Color targetColor = new Color(startColor.r, startColor.g, startColor.b, 1f);
+
+        while (timeElapsed < fadeDuration)
+        {
+            fadeImage.color = Color.Lerp(startColor, targetColor, timeElapsed / fadeDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        fadeImage.color = targetColor;
+
+        // Load the new scene after fade out is complete
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Call this to fade in (if you need it for transitions back to the main menu, for example)
+    public void FadeIn()
+    {
+        StartCoroutine(FadeInCoroutine());
+    }
+
+    private IEnumerator FadeInCoroutine()
+    {
+        float timeElapsed = 0f;
+        Color startColor = fadeImage.color;
+        Color targetColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
+
+        while (timeElapsed < fadeDuration)
+        {
+            fadeImage.color = Color.Lerp(startColor, targetColor, timeElapsed / fadeDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        fadeImage.color = targetColor;
+    }
+}
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // This method is for quitting the game (will only work in a built game)
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // This method will be called when the "Start" button is clicked
+    public void StartGame()
+    {
+        // Load the Game scene when the "Start" button is clicked
+        SceneManager.LoadScene("Game"); // Replace "Game" with the name of your game scene
+    }
+}
