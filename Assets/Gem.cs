ce27a43dc1e739bffca2527728b984bfcf0f72bb
DESKTOP-EDLB50K\littl <harrisonmuldowney06@gmail.com>
2025-01-16 09:44:50 +0000

    Added Gems

---
+++ Assets/Gem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Call the function to collect the gem
+            Collect();
+        }
+    }
+
+    void Collect()
+    {
+        // Disables the object when collected
+        gameObject.SetActive(false);
+
+       
+    }
+}
